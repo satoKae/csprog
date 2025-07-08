dc64e2cc146761f012d7f3f87a78167e9668b992
satokae <208755468+satokae@users.noreply.github.com>
2025-07-08 13:52:06 +0900

    sumdiff

---
+++ intro/SumDiff1.java
@@ -0,0 +1,6 @@
+public class SumDiff1 {
+  static public void main(String args[]) {
+    System.out.println(66 + 33);
+    System.out.println(66 - 33);
+  }
+}
---
+++ intro/SumDiff2.java
@@ -0,0 +1,6 @@
+public class SumDiff2 {
+  static public void main(String args[]) {
+    System.out.println("66 + 33 = " + (66 + 33));
+    System.out.println("66 - 33 = " + (66 - 33));
+  }
+}
