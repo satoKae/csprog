845c7f0401f8bfd76eb550e5ed80e2163683eeeb
satokae <208755468+satokae@users.noreply.github.com>
2025-07-08 13:55:09 +0900

    sumave

---
+++ intro/SumAve1.java
@@ -0,0 +1,12 @@
+public class SumAve1 {
+  static public void main(String args[]) {
+    int x;
+    int y;
+    x = 44;
+    y = 55;
+    System.out.println("xの値は" + x + "です");
+    System.out.println("yの値は" + y + "です");
+    System.out.println("合計は" + (x + y) + "です");
+    System.out.println("平均は" + (x + y) / 2 + "です");
+  }
+}
---
+++ intro/SumAve2.java
@@ -0,0 +1,12 @@
+public class SumAve2 {
+  static public void main(String args[]) {
+    double x;
+    double y;
+    x = 44.4;
+    y = 55.4;
+    System.out.println("xの値は" + x + "です");
+    System.out.println("yの値は" + y + "です");
+    System.out.println("合計は" + (x + y) + "です");
+    System.out.println("平均は" + (x + y) / 2 + "です");
+  }
+}
