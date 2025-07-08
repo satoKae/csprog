a64b06892fe4b1e923a5517fa7db53021348ab8f
satokae <208755468+satokae@users.noreply.github.com>
2025-07-08 13:41:02 +0900

    hello

---
+++ intro/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  static public void main(String args[]) {
+    System.out.print("Hello ");
+    System.out.println("world!");
+  }
+}
---
+++ intro/Hello1C.java
@@ -0,0 +1,5 @@
+public class Hello1C {
+  static public void main(String args[]) {
+    System.out.println("Hello " + "world!");
+  }
+}
