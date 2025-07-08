1de16584b3e82adf22e39fcabac5c3d0633028b3
satokae <208755468+satokae@users.noreply.github.com>
2025-07-08 13:59:30 +0900

    プチ演習 sumAve

---
+++ intro/SumAve3.java
@@ -0,0 +1,15 @@
+public class SumAve3 {
+  static public void main(String args[]) {
+    int x;
+    int y;
+    int z;
+    x = 10;
+    y = 20;
+    z = 30;
+    System.out.println("xの値は" + x + "です");
+    System.out.println("yの値は" + y + "です");
+    System.out.println("zの値は" + z + "です");
+    System.out.println("合計は" + (x + y + z) + "です");
+    System.out.println("平均は" + (x + y + z) / 3 + "です");
+  }
+}
---
+++ intro/SumAve4.java
@@ -0,0 +1,15 @@
+public class SumAve4 {
+  static public void main(String args[]) {
+    double x;
+    double y;
+    double z;
+    x = 11.1;
+    y = 22.3;
+    z = 26.6;
+    System.out.println("xの値は" + x + "です");
+    System.out.println("yの値は" + y + "です");
+    System.out.println("zの値は" + z + "です");
+    System.out.println("合計は" + (x + y + z) + "です");
+    System.out.println("平均は" + (x + y + z) / 3 + "です");
+  }
+}
